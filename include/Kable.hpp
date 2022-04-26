fc2c90686e23777ef4ae9d41c50c773ee991024b
Gerrit Erichsen <gerrit.erichsen@tuhh.de>
2022-04-26 08:02:27 +0200

    Decided against references for the target Plag in Kables...

--- include/Kable.hpp
+++ include/Kable.hpp
@@ -6,14 +6,15 @@
 class Kable
 {
 public:
-    Kable(PlagInterface & parent);
-    Kable(PlagInterface & parent, PlagInterface & target);
+    Kable(PlagInterface & parent, std::shared_ptr<PlagInterface> target = nullptr);
 
-    void translate(std::shared_ptr<Datagram> datagram);
+    void assignTarget(std::shared_ptr<PlagInterface> parent);
+
+    void transmit(std::shared_ptr<Datagram> datagram);
 
 private:
-    PlagInterface & m_parent;   //!< the source Plag (where Datagrams originate)
-    PlagInterface & m_target;   //!< the target Plag (where Datagrams are meant to be delivered to)
+    PlagInterface & m_parent;                   //!< the source Plag (where Datagrams originate)
+    std::shared_ptr<PlagInterface> m_target;    //!< the target Plag (where Datagrams are meant to be delivered to)
 };
 
 #endif // KABLE_HPP
--- src/Kable.cpp
+++ src/Kable.cpp
@@ -1,2 +1,23 @@
+// std include
+#include <iostream>
+
 // self include
 #include "Kable.hpp"
+
+using namespace std;
+
+Kable::Kable(PlagInterface & parent, std::shared_ptr<PlagInterface> target) :
+    m_parent(parent),
+    m_target(target)
+{
+}
+
+void Kable::assignTarget(std::shared_ptr<PlagInterface> target)
+{
+    m_target = target;
+}
+
+void Kable::transmit(shared_ptr<Datagram> datagram)
+{
+    cout << "Helloooo world!" << endl;
+}
